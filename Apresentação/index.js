3dd5fde1624581771982966fc06b90ed861865a0
thiagomelop <thiagomelop@yahoo.com.br>
2024-10-02 00:17:19 -0300

    comecei a estilizar a seção motivações, alterei a estrutura do html acrescentando algumas divs e separando melhor as tags

--- Apresentação/index.js
+++ Apresentação/index.js
@@ -14,3 +14,28 @@ function changeBackgroundImage() {
 }
 
 setInterval(changeBackgroundImage, 5000); // Muda a imagem a cada 5 segundos
+
+
+
+
+
+
+
+
+document.querySelectorAll('.motivacao-card-texto').forEach(card => {
+  card.addEventListener('mouseover', () => {
+    card.style.backgroundColor = '#000';
+    card.style.color = '#fff';
+    card.querySelector('p').style.display = 'block';
+  });
+
+  card.addEventListener('mouseout', () => {
+    card.style.backgroundColor = 'transparent';
+    card.style.color = '#000';
+    card.querySelector('p').style.display = 'none';
+  });
+});
+
+
+
+
