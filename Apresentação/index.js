ec915bbe05f20d17a4a9cbc20dfbaf3fbffe0bb6
thiagomelop <thiagomelop@yahoo.com.br>
2024-10-02 00:24:51 -0300

    alterei o valor de um padding da classe inicial

--- Apresentação/index.js
+++ Apresentação/index.js
@@ -1,19 +1,19 @@
-const images = [
-  './img/img-culinaria1.jpg',
-  './img/img-culinaria2.jpg',
-  './img/img-viagem.jpg',
-  './img/img-viagem1.jpg'
-];
+// const images = [
+//   './img/img-culinaria1.jpg',
+//   './img/img-culinaria2.jpg',
+//   './img/img-viagem.jpg',
+//   './img/img-viagem1.jpg'
+// ];
 
-let currentIndex = 0;
+// let currentIndex = 0;
 
-function changeBackgroundImage() {
-  const card = document.querySelector('.card-background');
-  card.style.backgroundImage = `url(${images[currentIndex]})`;
-  currentIndex = (currentIndex + 1) % images.length;
-}
+// function changeBackgroundImage() {
+//   const card = document.querySelector('.card-background');
+//   card.style.backgroundImage = `url(${images[currentIndex]})`;
+//   currentIndex = (currentIndex + 1) % images.length;
+// }
 
-setInterval(changeBackgroundImage, 5000); // Muda a imagem a cada 5 segundos
+// setInterval(changeBackgroundImage, 5000); // Muda a imagem a cada 5 segundos
 
 
 
